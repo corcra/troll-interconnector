93ca4754a363a8e1d744e339fb29fcf15eb40bab
Stephanie Hyland <steph.hyland@gmail.com>
2015-06-11 21:08:37 -0400

    includes authentication, API magix with tweepy

--- bot.py
+++ bot.py
@@ -2,7 +2,21 @@
 # One script to rule them and in the darkness troll them.
 
 import madbot
+import tweepy
+from credz import consumer_key, consumer_secret, access_token, access_token_secret
 
+# --- get set up with the twitters --- #
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
+
+# example from the tweepy page!
+#public_tweets = api.home_timeline()
+#for tweet in public_tweets:
+#    print tweet.text
+
+# --- STUFF --- #
 selector = '#hashtag'
 
 # --- option one: find target, then find match --- #
@@ -20,3 +34,6 @@ target, match = madbot.pair_trolls(targets)
 # --- now manage the mischief --- #
 tweet = madbot.form_connection(match, target, selector)
 # ... and then you send the tweet, somehow (API-implementation specific)
+#
+
+
