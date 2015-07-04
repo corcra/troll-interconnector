f96f979d6143b88ffbfd4e7ab60c35a4bed9e8e8
alourenco <alexander.lourenco@gmail.com>
2015-07-04 13:38:54 +0000

    forgot init.py in database folder

--- tests/tests_database.py
+++ tests/tests_database.py
@@ -1,4 +1,4 @@
-import unittest
+import unittest, sys, os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
