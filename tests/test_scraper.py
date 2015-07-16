c537716febeddaf6411a2d042bbf82686498c0a4
alourenco <alexander.lourenco@gmail.com>
2015-07-16 08:39:38 +0000

    no reason to have tests just for database, rolled into scraper tests

--- tests/test_scraper.py
+++ tests/test_scraper.py
@@ -7,24 +7,25 @@ import database.models as models
 # tests for the scraper
 
 
-engine = create_engine('sqlite:///:memory:', echo=True)
-models.Base.metadata.create_all(bind=engine)
-Session = sessionmaker(bind=engine)
-db_session = Session()
-
-    
-    
 class ScraperTests(unittest.TestCase):
     # set up the test database
 
+    def setUp(self):
+        engine = create_engine('sqlite:///:memory:', echo=True)
+        models.Base.metadata.create_all(bind=engine)
+        Session = sessionmaker(bind=engine)
+        self.db_session = Session()
+        
     def test_db_connection(self):
         # make sure db works
         test_user = models.TwitterUser(id="12345", name="test user")
-        db_session.add(test_user)
-        db_session.commit()
-        test_retrieve = db_session.query(models.TwitterUser).filter_by(
+        self.db_session.add(test_user)
+        self.db_session.commit()
+        test_retrieve = self.db_session.query(models.TwitterUser).filter_by(
             name="test user").first()
         self.assertEqual("test user", test_retrieve.name)
+        
+    
         
 if __name__ == '__main__':
     unittest.main()
