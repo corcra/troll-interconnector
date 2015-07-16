6b4803fd250b3077ba36d5740187a4ddb38f2656
Alexander Lourenco <alexander.lourenco@gmail.com>
2015-07-16 09:32:17 +0000

    started tweet_scraper, added settings.py

---
+++ settings.py
@@ -0,0 +1,17 @@
+#put all our settings here
+
+# don't want the consumer token public. Importing from my home directory,
+#but you can change this. Should standardize this eventually for deployment
+
+import sys, os
+
+secretsdir = os.path.join(os.getenv("HOME"), "sensitive_data")
+sys.path.append(secretsdir)
+from secret_stuff import *
+
+# TWITTER_CONSUMER_KEY and TWITTER_CONSUMER_SECRET are now defined
+
+
+
+
+
--- tweet_scraper.py
+++ tweet_scraper.py
@@ -1,3 +1,17 @@
 from database import *
-from models import *
+import tweepy
+from settings import *
+import urllib3
 
+#disable warnings in case somebody (like me) is running 
+#a Python version < 2.7.9
+
+urllib3.disable_warnings()
+
+auth = tweepy.OAuthHandler(TWITTER_CONSUMER_KEY, TWITTER_CONSUMER_SECRET)
+try:
+    redirect_url = auth.get_authorization_url()
+except tweepy.TweepError:
+    print 'Error! Failed to get request token.'
+    
+    
