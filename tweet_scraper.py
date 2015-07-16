ad69c80f52fea59be54a5f43322a26dd4ef0d507
Alexander Lourenco <alexander.lourenco@gmail.com>
2015-07-16 21:43:45 +0000

    started auth stuff, successful auth

--- tweet_scraper.py
+++ tweet_scraper.py
@@ -2,16 +2,24 @@ from database import *
 import tweepy
 from settings import *
 import urllib3
+urllib3.disable_warnings()
 
 #disable warnings in case somebody (like me) is running 
 #a Python version < 2.7.9
 
-urllib3.disable_warnings()
+
+def TweepyAuth(consumer_key, consumer_secret):
+    #only has to be run once, then you can just put the token in secret_data
+    
+    t_auth = tweepy.OAuthHandler(TWITTER_CONSUMER_KEY, TWITTER_CONSUMER_SECRET)
+    auth_url = t_auth.get_authorization_url()
+    print 'Please authorize: ' + auth_url
+    verifier = raw_input('PIN: ').strip()
+    t_auth.get_access_token(verifier)
+    print t_auth
 
 auth = tweepy.OAuthHandler(TWITTER_CONSUMER_KEY, TWITTER_CONSUMER_SECRET)
-try:
-    redirect_url = auth.get_authorization_url()
-except tweepy.TweepError:
-    print 'Error! Failed to get request token.'
-    
-    
+auth.set_access_token(TWITTER_ACCESS_TOKEN, TWITTER_ACCESS_SECRET)
+api = tweepy.API(auth)
+user = api.me()
+print user.name
