85844320ac974dd908b28f0fd973dce48403ead3
Alexander Lourenco <alexander.lourenco@gmail.com>
2015-07-19 15:44:37 +0000

    added in the test.db by accident, no reason for it to be in remote repo

--- tweet_scraper.py
+++ tweet_scraper.py
@@ -13,6 +13,10 @@ urllib3.disable_warnings()
 # migrate to that.
 class TweetScraper(object):
     
+    auth = tweepy.OAuthHandler(TWITTER_CONSUMER_KEY, TWITTER_CONSUMER_SECRET)
+    auth.set_access_token(TWITTER_ACCESS_TOKEN, TWITTER_ACCESS_SECRET)
+    api = tweepy.API(auth)
+    
     def TweepyAuth(self, consumer_key, consumer_secret):
         # only has to be run once then you can just put the token in secret_data
         # would be best be broken out into some sort of utils library later
@@ -23,11 +27,51 @@ class TweetScraper(object):
         verifier = raw_input('PIN: ').strip()
         t_auth.get_access_token(verifier)
         return t_auth
+    
+    def extract_tweet(self, tweet):
+        ''' pass this either a Tweepy SearchObject or a twitter id_str: it then
+        either parses the SearchObject or fetches the associated id_str tweet'''
+        if isinstance(tweet, basestring):
+            # if tweet is a string, that means we need to fetch the tweet
+            result = self.api.get_status(tweet)
+        else:
+            result = tweet
+        tweet_author = db.session.query(models.TwitterUser).get(
+            result.user.id)
+        if not tweet_author:
+            # new user previously unseen, add to db
+            tweet_author = models.TwitterUser(id=result.user.id, 
+                name=result.user.screen_name)
+            db.session.add(tweet_author)
+        tweet_result = models.Tweet(id_str=result.id_str, 
+            content=result.text, author=tweet_author)
+        if result.in_reply_to_status_id_str:
+            # if it's a reply, we want to know what tweet it's replying 
+            # to
+            tweet_result.reply_to_tweet_id_str = \
+                result.in_reply_to_status_id_str
+        if result.in_reply_to_user_id_str:
+            tweet_result.reply_to_user_id_str = \
+                result.in_reply_to_user_id_str
+        # if any users are mentioned, probably interesting- let's get 'em
+        if "id" in result.entities["user_mentions"]:
+            for mentioned_user in result.entities["user_mentions"]:
+                mentioned_author = db.session.query(
+                    models.TwitterUser).get(
+                        mentioned_user["id"])
+                if not mentioned_author:
+                    mentioned_author = models.TwitterUser(
+                        id=mentioned_user["id"],
+                        name=mentioned_user["screen_name"])
+                db.session.add(mentioned_author)
+                    
+                
+                # start all the hashtag stuff here
+                
+        db.session.add(tweet_result)
+        # commit it all to the db
+        db.session.commit()
 
-
-    auth = tweepy.OAuthHandler(TWITTER_CONSUMER_KEY, TWITTER_CONSUMER_SECRET)
-    auth.set_access_token(TWITTER_ACCESS_TOKEN, TWITTER_ACCESS_SECRET)
-    api = tweepy.API(auth)
     
     def search_tweets(self, query):
         """ Will suck down and store results of a search in the db """
@@ -40,41 +84,9 @@ class TweetScraper(object):
             if tweet_result:
                 # entry already exists, skip it
                 continue
-            tweet_author = db.session.query(models.TwitterUser).get(
-                result.user.id)
-            if not tweet_author:
-                # new user previously unseen, add to db
-                tweet_author = models.TwitterUser(id=result.user.id, 
-                    name=result.user.screen_name)
-                db.session.add(tweet_author)
-            tweet_result = models.Tweet(id_str=result.id_str, 
-                content=result.text, author=tweet_author)
-            if result.in_reply_to_status_id_str:
-                # if it's a reply, we want to know what tweet it's replying 
-                # to
-                tweet_result.reply_to_tweet_id_str = \
-                    result.in_reply_to_status_id_str
-            if result.in_reply_to_user_id_str:
-                tweet_result.reply_to_user_id_str = \
-                    result.in_reply_to_user_id_str
-            # if any users are mentioned, probably interesting- let's get 'em
-            if "id" in result.entities["user_mentions"]:
-                for mentioned_user in result.entities["user_mentions"]:
-                    mentioned_author = db.session.query(
-                        models.TwitterUser).get(
-                            mentioned_user["id"])
-                    if not mentioned_author:
-                        mentioned_author = models.TwitterUser(
-                            id=mentioned_user["id"],
-                            name=mentioned_user["screen_name"])
-                    db.session.add(mentioned_author)
-                    
-                
-                # start all the hashtag stuff here
-                
-            db.session.add(tweet_result)
-            # commit it all to the db
-            db.session.commit()
+            else:
+                print "test"
+                self.extract_tweet(result)
             
 t_scraper = TweetScraper()
-t_scraper.search_tweets("obama")
+t_scraper.search_tweets("Jade Helm 15")
