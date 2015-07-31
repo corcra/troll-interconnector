1b9080ae0add198d0d5d78eb8f219345424d3580
Stephanie Hyland <steph.hyland@gmail.com>
2015-07-31 12:39:28 -0400

    tidied up troll score, added ideology: both just set an attribute

--- madbot/tweep.py
+++ madbot/tweep.py
@@ -3,6 +3,7 @@
 # 
 # HOW TO OOP ?
 
+
 import numpy as np
 
 class tweep(object):
@@ -19,11 +20,23 @@ class tweep(object):
         self.tweets = tweets
         self.followers = followers
         # and so on and so forth
-    def get_troll_score(self):
+    def calculate_troll_score(self):
         """
         define a 'troll probability' (in [0, 1])
         """
         # magic machine learning goes here
         # so spooky and magical
         score = np.random.random()
-        return score
+        self.score = score
+        return True
+    def calculate_ideology(self):
+        """
+        for now I'm assuming ideology is a vector
+        NOTE: we may not want to go down this route
+        ... depends on if we have any good idea of how to obtain these vectors
+        """
+        # even more magical machine learning
+        ideology_dimension = 5
+        ideology = np.random.normal(ideology_dimension)
+        self.ideology = ideology
+        return True
