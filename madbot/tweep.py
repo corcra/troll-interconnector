fcad8b1fd8d8a8451d8cc98968ae90451b2e4c7d
Stephanie Hyland <steph.hyland@gmail.com>
2015-07-31 12:33:02 -0400

    class for twitter users, extremely rough for now

---
+++ madbot/tweep.py
@@ -0,0 +1,29 @@
+#!/bin/python
+# Class for tweeps
+# 
+# HOW TO OOP ?
+
+import numpy as np
+
+class tweep(object):
+    """
+    oh god
+    """
+    def __init__(self, params):
+        """
+        wat
+        """
+        # need interaction with the database here
+        tweets = ['placeholder', 'placeholder']
+        followers = ['placeholder', 'placeholder']
+        self.tweets = tweets
+        self.followers = followers
+        # and so on and so forth
+    def get_troll_score(self):
+        """
+        define a 'troll probability' (in [0, 1])
+        """
+        # magic machine learning goes here
+        # so spooky and magical
+        score = np.random.random()
+        return score
